82205381182669cfbea11bdba571a0ef0233fc87
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2020-10-09 09:45:45 +0700

    Update transection1.js

--- src/Component/transection1.js
+++ src/Component/transection1.js
@@ -1,6 +1,6 @@
 import React  from 'react';
 import "../css/chillcard.css";
-import { ReactComponent as Logo } from "../img/logo3.png";
+import { ReactComponent as Logo } from "../img/logo3.svg";
 import { Button } from "@material-ui/core";
 import axios from "axios";
 // import { useHistory } from "react-router-dom";
